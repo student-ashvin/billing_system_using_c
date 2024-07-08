b9fcb34235946239ea62b10cfd7d26b376ea104c
student-ashvin <144105750+student-ashvin@users.noreply.github.com>
2024-07-08 20:47:23 +0530

    Add files via upload

---
+++ billing by using c language.c
@@ -0,0 +1,116 @@
+#includde<stdio.h>
+int main()
+{
+	char name[50];
+	int phone_number;
+	int customer_id;
+	
+	
+	int body_soap;
+	int hair_cream;
+	int hair_spray;
+	int body_spray;
+	
+	
+	int sugar;
+	int tea;
+	int coffee;
+	int rice;
+	int wheat;
+	
+	it pepsi;
+	int sprite;
+	int coke;
+	int mojitos;
+	int thumbs_up;
+	
+
+	
+	int total;
+	int cosmetics_total;
+	int  grocery_total;
+	int beverage_total;
+	
+	
+	printf("----------------\n");
+	printf("BILLING SYSTUM\n");
+	printf("------------\n");
+	printf("Customer  details\n\n");
+
+printf("Customer name:");
+scanf("%s",&name);
+printf("customer number:");
+scanf("%d",&phone_number);
+printf("Customer id:");
+printf("%d",&Customer_id);
+
+printf("------------\n");
+printf("COSMETICS\n\n");
+
+printf("body soap (Rs 10) :");
+scanf("%d,"&body_soap);
+printf("hair cream(Rs 25):");
+scanf("%d",&hair_cream);
+printf("body spray(Rs 50) :");
+scanf("%d",&body_spray);
+printf("hair spray(Rs 50) :");
+scanf("%d",&hair_spray);
+
+printf("---------------\n");
+
+printf("groceries\n\n");
+
+printf("sugar(Rs 100) :")
+scanf("%d",&sugar);
+printf("tea(Rs 20) :);
+scanf("%d",&tea);
+printf("coffee (rs 35):");
+scanf("%d",&coffee);
+printf("wheat (rs 60) :");
+scanf("%d",&wheat);
+printf("rice(rs 80) :");
+scanf("%d",&rice);
+
+printf("-------------\n");
+
+
+printf("BREVAREGES\n\n");
+
+printf("pepsi(rs 30)");
+scanf("%d",&pepsi);
+printf("sprite(rs30)");
+scanf("%d",&sprite);
+printf("coke(rs 30)");
+scanf("%d",&coke);
+printf("mojitos(rs30)");
+scanf("%d",&mojitos);
+printf("thumbs up(rs30)");
+scanf("%d",&thumbs_up);
+
+
+printf("------------\n");
+
+
+int boso;
+int hc;
+int hs;
+int bosp;
+
+
+boso = 10* body_soap;
+hc = 25* hair_cream:
+	hs = 50* hair_spray;
+	bosp = 50* body_spray;
+	cosmetics_total = boso+hs+hc+bosp;
+	
+		
+	
+
+
+
+
+
+
+
+}
+
